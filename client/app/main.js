eb1dc70f2b2ed66b392de32c7f970a916f9fad7b
Thuong Ho <thuong.t.ho@gmail.com>
2016-05-28 23:18:52 -0700

    add isomorphic validation code that works on both server and client

--- client/app/main.js
+++ client/app/main.js
@@ -1,7 +1,10 @@
 'user strict';
 
 var utils = require('./utils');
+// using isomorphic validation on client
+var iso = require('../../portable');
 
 console.log('Client side code started');
+console.log(iso.validateId('isdjlmsld'));
 
 utils.count();
